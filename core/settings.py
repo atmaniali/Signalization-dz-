45acc5eb0554002cbc342a0168a37f9452c302e2
atmaniali <atmaniali97@gmail.com>
2023-11-06 00:21:21 +0100

    modefiying file

--- core/settings.py
+++ core/settings.py
@@ -92,10 +92,20 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "ecommerce",
+        "USER": "root",
+        "PASSWORD": "admin",  # pw_generate: N(*VCZ_8YP0D
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
